0329123f6165144b4101d6a6e9aa731af1ed84f1
Henrik Rypkema <HenrikR@users.noreply.github.com>
2018-06-26 13:17:38 -0400

    Support x-axis threshold with indicator and multiple charts

--- src/scripts/chartist-plugin-threshold.js
+++ src/scripts/chartist-plugin-threshold.js
@@ -1,5 +1,5 @@
 /**
- * Chartist.js plugin to display a data label on top of the points in a line chart.
+ * Chartist.js plugin to divide your Line or Bar chart with a threshold.
  *
  */
 /* global Chartist */
@@ -8,6 +8,7 @@
 
   var defaultOptions = {
     threshold: 0,
+    showIndicator: false,
     classNames: {
       aboveThreshold: 'ct-threshold-above',
       belowThreshold: 'ct-threshold-below'
@@ -21,10 +22,54 @@
   function createMasks(data, options) {
     // Select the defs element within the chart or create a new one
     var defs = data.svg.querySelector('defs') || data.svg.elem('defs');
-    // Project the threshold value on the chart Y axis
-    var projectedThreshold = data.chartRect.height() - data.axisY.projectValue(options.threshold) + data.chartRect.y2;
     var width = data.svg.width();
     var height = data.svg.height();
+    var projectedThreshold, aboveRect, belowRect;
+
+    if (!data.options.horizontalBars) {
+      // Project the threshold value on the chart Y axis
+      projectedThreshold =
+        data.chartRect.height() -
+        data.axisY.projectValue(options.threshold) +
+        data.chartRect.y2;
+
+      aboveRect = {
+        x: 0,
+        y: 0,
+        width: width,
+        height: projectedThreshold,
+        fill: 'white'
+      };
+
+      belowRect = {
+        x: 0,
+        y: projectedThreshold,
+        width: width,
+        height: height - projectedThreshold,
+        fill: 'white'
+      };
+    } else {
+      // Project the threshold value on the chart X axis
+      projectedThreshold =
+        data.axisX.projectValue(options.threshold) +
+        data.chartRect.x1;
+
+      aboveRect = {
+        x: projectedThreshold,
+        y: 0,
+        width: width - projectedThreshold,
+        height: height,
+        fill: 'white'
+      };
+
+      belowRect = {
+        x: 0,
+        y: 0,
+        width: projectedThreshold,
+        height: height,
+        fill: 'white'
+      };
+    }
 
     // Create mask for upper part above threshold
     defs
@@ -33,15 +78,10 @@
         y: 0,
         width: width,
         height: height,
-        id: options.maskNames.aboveThreshold
+        maskUnits: 'userSpaceOnUse',
+        id: options.maskNames.aboveThresholdID
       })
-      .elem('rect', {
-        x: 0,
-        y: 0,
-        width: width,
-        height: projectedThreshold,
-        fill: 'white'
-      });
+      .elem('rect', aboveRect);
 
     // Create mask for lower part below threshold
     defs
@@ -50,15 +90,52 @@
         y: 0,
         width: width,
         height: height,
-        id: options.maskNames.belowThreshold
+        maskUnits: 'userSpaceOnUse',
+        id: options.maskNames.belowThresholdID
       })
-      .elem('rect', {
-        x: 0,
-        y: projectedThreshold,
-        width: width,
-        height: height - projectedThreshold,
-        fill: 'white'
-      });
+      .elem('rect', belowRect);
+
+    // Show a line indicator and label where the threshold is
+    if (options.showIndicator) {
+      // Select the group element within the chart or create a new one
+      var group = data.svg.querySelector('g.ct-indicator') || data.svg.elem('g').addClass('ct-indicator');
+      var textX, textY, line;
+      
+      // Project the threshold values for the line and label
+      if (data.options.horizontalBars) {
+        textX = projectedThreshold;
+        textY = 10;
+        line = {
+          x1: projectedThreshold,
+          x2: projectedThreshold,
+          y1: 0,
+          y2: height,
+        };
+      } else {
+        textX = 10;
+        textY = projectedThreshold;
+        line = {
+          x1: 0,
+          x2: width,
+          y1: projectedThreshold,
+          y2: projectedThreshold,
+        };
+      }
+
+      group
+        .elem('text', {
+          x: textX,
+          y: textY,
+          style: 'text-anchor: middle',
+        }, 'ct-label ct-value-label')
+        .text(options.threshold);
+
+      group
+        .elem('line',
+          line,
+          'ct-grid ct-horizontal'
+        );
+    }
 
     return defs;
   }
@@ -67,6 +144,10 @@
   Chartist.plugins.ctThreshold = function (options) {
 
     options = Chartist.extend({}, defaultOptions, options);
+
+    // Ensure mask names are unique
+    options.maskNames.aboveThresholdID = options.maskNames.aboveThreshold + '-' + Math.random().toString(36).substr(2, 9);
+    options.maskNames.belowThresholdID = options.maskNames.belowThreshold + '-' + Math.random().toString(36).substr(2, 9);
 
     return function ctThreshold(chart) {
       if (chart instanceof Chartist.Line || chart instanceof Chartist.Bar) {
@@ -77,14 +158,18 @@
             data.element.addClass(
               data.value.y >= options.threshold ? options.classNames.aboveThreshold : options.classNames.belowThreshold
             );
-          } else if (data.type === 'line' || data.type === 'bar' || data.type === 'area') {
+          } else if (
+            data.type === 'line' ||
+            data.type === 'bar' ||
+            data.type === 'area'
+          ) {
             // Cloning the original line path, mask it with the upper mask rect above the threshold and add the
             // class for above threshold
             data.element
               .parent()
               .elem(data.element._node.cloneNode(true))
               .attr({
-                mask: 'url(#' + options.maskNames.aboveThreshold + ')'
+                mask: 'url(#' + options.maskNames.aboveThresholdID + ')'
               })
               .addClass(options.classNames.aboveThreshold);
 
@@ -92,7 +177,7 @@
             // for blow threshold
             data.element
               .attr({
-                mask: 'url(#' + options.maskNames.belowThreshold + ')'
+                mask: 'url(#' + options.maskNames.belowThresholdID + ')'
               })
               .addClass(options.classNames.belowThreshold);
           }
@@ -104,5 +189,5 @@
         });
       }
     };
-  }
+  };
 }(window, document, Chartist));
